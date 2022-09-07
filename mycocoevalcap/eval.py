3110965ce68c67e5373b519bf4a18ee54a98050b
jxchen <knosikay@gmail.com>
2022-09-07 21:15:37 +0800

    reorganize file

--- mycocoevalcap/eval.py
+++ mycocoevalcap/eval.py
@@ -1,4 +1,5 @@
 __author__ = 'tylin'
+from unittest import skip
 from .tokenizer.ptbtokenizer import PTBTokenizer
 from .bleu.bleu import Bleu
 from .meteor.meteor import Meteor
@@ -8,13 +9,35 @@ from .spice.spice import Spice
 
 
 class COCOEvalCap:
-    def __init__(self, coco, cocoRes):
+    def __init__(self, coco, cocoRes, skip_metrics=None):
+        # =================================================
+        # Set up scorers
+        # =================================================
+        print('setting up scorers...')
+        self.scorers = [
+            (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
+            (Meteor(),"METEOR"),
+            (Rouge(), "ROUGE_L"),
+            (Cider(), "CIDEr"),
+            (Spice(), "SPICE")
+        ]
+        self.reset(coco, cocoRes)
+        self.reset_skip_metrics(skip_metrics)
+
+
+    def reset(self, coco, cocoRes):
         self.evalImgs = []
         self.eval = {}
         self.imgToEval = {}
         self.coco = coco
         self.cocoRes = cocoRes
         self.params = {'image_id': coco.getImgIds()}
+
+    def reset_skip_metrics(self, skip_metrics=None):
+        if skip_metrics is None:
+            self.skip_metrics = set()
+        else:
+            self.skip_metrics = set(skip_metrics)
 
     def evaluate(self):
         imgIds = self.params['image_id']
@@ -28,38 +51,30 @@ class COCOEvalCap:
         # =================================================
         # Set up scorers
         # =================================================
-        print('tokenization...')
+        # print('tokenization...')
         tokenizer = PTBTokenizer()
         gts  = tokenizer.tokenize(gts)
         res = tokenizer.tokenize(res)
 
-        # =================================================
-        # Set up scorers
-        # =================================================
-        print('setting up scorers...')
-        scorers = [
-            (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
-            (Meteor(),"METEOR"),
-            (Rouge(), "ROUGE_L"),
-            (Cider(), "CIDEr"),
-            (Spice(), "SPICE")
-        ]
+        scorers = self.scorers
 
         # =================================================
         # Compute scores
         # =================================================
         for scorer, method in scorers:
-            print('computing %s score...'%(scorer.method()))
+            if scorer.method() in self.skip_metrics:
+                continue
+            # print('computing %s score...'%(scorer.method()))
             score, scores = scorer.compute_score(gts, res)
             if type(method) == list:
                 for sc, scs, m in zip(score, scores, method):
                     self.setEval(sc, m)
                     self.setImgToEvalImgs(scs, gts.keys(), m)
-                    print("%s: %0.3f"%(m, sc))
+                    # print("%s: %0.3f"%(m, sc))
             else:
                 self.setEval(score, method)
                 self.setImgToEvalImgs(scores, gts.keys(), method)
-                print("%s: %0.3f"%(method, score))
+                # print("%s: %0.3f"%(method, score))
         self.setEvalImgs()
 
     def setEval(self, score, method):
--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_namespace_packages, find_packages
 
 # Prepend pycocoevalcap to package names
-package_names = ['pycocoevalcap.'+p for p in find_namespace_packages()]
+# package_names = ['mycocoevalcap.'+p for p in find_namespace_packages()]
 
 with open("README.md", "r") as fh:
     readme = fh.read()
 
 setup(
-    name='pycocoevalcap',
+    name='mycocoevalcap',
     version=1.2,
-    maintainer='salaniz',
+    maintainer='jxchen',
     description="MS-COCO Caption Evaluation for Python 3",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/salaniz/pycocoevalcap",
-    packages=['pycocoevalcap']+package_names,
-    package_dir={'pycocoevalcap': '.'},
+    # url="https://github.com/salaniz/pycocoevalcap",
+    # packages=['mycocoevalcap']+package_names,
+    packages=find_packages(),
+    # package_dir={'mycocoevalcap': '.'},
     package_data={'': ['*.jar', '*.gz']},
     install_requires=['pycocotools>=2.0.2'],
     python_requires='>=3'
